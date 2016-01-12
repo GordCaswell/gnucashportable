051696750072a5ad564b39c74bfd222e29901950
Gord Caswell <GordCaswell@users.noreply.github.com>
2016-01-12 12:37:06 -0600

    Base app update to 2.6.11

--- App/GnuCash/share/gnucash/scm/build-config.scm
+++ App/GnuCash/share/gnucash/scm/build-config.scm
@@ -1,6 +1,6 @@
 
-(define gnc:version "2.6.10")
+(define gnc:version "2.6.11")
 
 ;; Automatically generated defaults (don't use these directly --
 ;; they're used during actual initialization elsewhere)
-(define gnc:_install-doc-path_ '("c:/gcdev/gnucash-2.6.10/gnucash/inst/share"))
+(define gnc:_install-doc-path_ '("c:/gcdev/gnucash-2.6.11/gnucash/inst/share"))
